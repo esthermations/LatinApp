cf9686fbd0c6fb5a857f5990d3223f5f755719a4
Esther O'Keefe <esther@okeefe.earth>
2021-02-14 18:16:21 +1100

    Update website design and improve UX

--- src/LatinApp.nim
+++ src/LatinApp.nim
@@ -1,35 +1,54 @@
 import
   dom,
   jsconsole,
-  json
+  json,
+  strformat
+
 
 const
-  defaultBgColour = "#B57EDC"
-  defaultFgColour = "#000000"
-  defaultFont = "48px Libre Baskerville"
+  defaultResult = """<div class="noResult">nūlla resultāta</div>"""
 
 var
   searchBox: InputElement
   resultBox: Element
+  loadButton: Element
   jsonData: JsonNode = nil
 
+proc informUser(s: string) =
+  console.log(s)
+
+### Search our JSON data for the given string
 proc search(s: cstring): cstring =
   assert jsonData != nil
   return jsonData{$s}.getStr()
 
+### Perform a search whenever the user enters a letter into the search box
 proc onSearchInput() {.exportc.} =
   assert searchBox != nil
   var searchString = searchBox.value
-  resultBox.innerHTML = search(searchString)
+  var result = search(searchString)
+  if result.len == 0:
+    resultBox.innerHTML = defaultResult
+  else:
+    resultBox.innerHTML = fmt"""<div class="result">{$result}</div>"""
 
-# Entry point for the whole .... thing
-proc onPageLoad() {.exportc.} =
-  console.log("Initialising!")
-
+### Parse JSON from the string in memory
+proc onLoadButtonPressed() {.exportc.} =
   const jsonString = staticRead("../out.json")
   jsonData = parseJson(jsonString)
+  loadButton.innerHTML = "Factum!"
+  searchBox.disabled = false
+  loadButton.disabled = true
+
+### Entry point for the whole .... thing
+proc onPageLoad() {.exportc.} =
+  informUser "Page has loaded!"
 
   searchBox = document.getElementById("searchBox").InputElement
-  resultBox = document.getElementById("resultBox")
+  searchBox.disabled = true # Enabled when we load the Json
 
-  console.log("Initialised!")
+  resultBox = document.getElementById("resultBox")
+  resultBox.innerHTML = defaultResult
+
+  loadButton = document.getElementById("loadButton")
+  loadButton.innerHTML = "Data JSON carricāre"
